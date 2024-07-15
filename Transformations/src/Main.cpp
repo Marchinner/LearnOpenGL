5da60cca843a07a1d2404c36a967bdfd9ec669f8
Felipe Freitas <felipefreitasdev@gmail.com>
2024-07-15 20:01:23 -0300

    various triangles with imgui

--- Transformations/src/Main.cpp
+++ Transformations/src/Main.cpp
@@ -15,11 +15,30 @@ const char* TITLE = "Triangulo com textura";
 // Function Prototypes
 void framebufferSizeCallback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 
 void framebufferSizeCallback(GLFWwindow* window, int width, int height)
 {
 	glViewport(0, 0, width, height);
 }
+
+// Camera
+glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+bool firstMouse = true;
+float yaw = -90.0f;	// yaw is initialized to -90.0 degrees since a yaw of 0.0 results in a direction vector pointing to the right so we initially rotate a bit to the left.
+float pitch = 0.0f;
+float lastX = 800.0f / 2.0;
+float lastY = 600.0 / 2.0;
+float fov = 45.0f;
+
+
+// delta time
+float deltaTime = 0.0f;	// Time between current frame and last frame
+float lastFrame = 0.0f; // Time of last frame
+
 
 int main()
 {
@@ -48,7 +67,9 @@ int main()
 	glfwMakeContextCurrent(window);
 	// Set the window resize callback
 	glfwSetWindowSizeCallback(window, framebufferSizeCallback);
-
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+	glfwSetCursorPosCallback(window, mouse_callback);
+	glfwSetScrollCallback(window, scroll_callback);
 	// Initialize GLAD
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
 	{
@@ -68,7 +89,7 @@ int main()
 		-0.5f, -0.5f, 0.5f,  1.0f, 0.0f, 0.0f,  0.0f, 0.0f,
 		 0.5f, -0.5f, 0.5f,  0.0f, 1.0f, 0.0f,  1.0f, 0.0f,
 		 0.5f, -0.5f,-0.5f,  0.0f, 0.0f, 1.0f,  1.0f, 0.0f,
-		-0.5f, -0.5f,-0.5f,  1.0f, 0.0f, 0.0f,  0.0f, 0.0f, 
+		-0.5f, -0.5f,-0.5f,  1.0f, 0.0f, 0.0f,  0.0f, 0.0f,
 		 0.0f,  0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  0.5f, 1.0f
 	};
 
@@ -157,7 +178,7 @@ int main()
 	};
 
 	float viewPosition[3]{
-		0.0f, 0.0f, -3.0f
+		0.0f, 0.0f, 3.0f
 	};
 
 	float color[4]{
@@ -172,6 +193,19 @@ int main()
 
 	float fovy = 0.0f;
 
+	glm::vec3 cubePositions[] = {
+	glm::vec3(0.0f,  0.0f,  0.0f),
+	glm::vec3(2.0f,  5.0f, -15.0f),
+	glm::vec3(-1.5f, -2.2f, -2.5f),
+	glm::vec3(-3.8f, -2.0f, -12.3f),
+	glm::vec3(2.4f, -0.4f, -3.5f),
+	glm::vec3(-1.7f,  3.0f, -7.5f),
+	glm::vec3(1.3f, -2.0f, -2.5f),
+	glm::vec3(1.5f,  2.0f, -2.5f),
+	glm::vec3(1.5f,  0.2f, -1.5f),
+	glm::vec3(-1.3f,  1.0f, -1.5f)
+	};
+
 	glEnable(GL_DEPTH_TEST);
 	// Main Loop
 	while (!glfwWindowShouldClose(window))
@@ -179,30 +213,36 @@ int main()
 		// Process Inputs
 		processInput(window);
 
+		float currentFrame = glfwGetTime();
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
+
 		// Set the clear window color
 		glClearColor(clearColor[0], clearColor[1], clearColor[2], 1.0f);
 		// Clear the color buffer
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-		rotationAngle = glfwGetTime() * 100.0f;
-		glm::mat4 model = glm::mat4(1.0f);
-		model = glm::rotate(model, glm::radians(rotationAngle), glm::vec3(rotationAxis[0], rotationAxis[1], rotationAxis[2]));
-
-		glm::mat4 view = glm::mat4(1.0f);
-		view = glm::translate(view, glm::vec3(viewPosition[0], viewPosition[1], viewPosition[2]));
-
-		glm::mat4 projection;
-		projection = glm::perspective(glm::radians(45.0f), (float)WIDTH / (float)HEIGHT, 0.1f, 100.0f);
-
-
+		glm::mat4 view;
+		view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
 
 		// Render
 		myShader.use();
-		myShader.setMat4("model", model);
-		myShader.setMat4("view", view);
-		myShader.setMat4("projection", projection);
-		glBindVertexArray(VAO);
-		glDrawElements(GL_TRIANGLES, sizeof(index), GL_UNSIGNED_INT, 0);
+
+		for (int i = 0; i < 10; i++)
+		{
+			rotationAngle = glfwGetTime() * 100.0f;
+			glm::mat4 model = glm::mat4(1.0f);
+			model = glm::translate(model, cubePositions[i]);
+			model = glm::rotate(model, glm::radians(rotationAngle), glm::vec3(rotationAxis[0], rotationAxis[1], rotationAxis[2]));
+
+			glm::mat4 projection;
+			projection = glm::perspective(glm::radians(fov), (float)WIDTH / (float)HEIGHT, 0.1f, 100.0f);
+
+			myShader.setMat4("model", model);
+			myShader.setMat4("view", view);
+			myShader.setMat4("projection", projection);
+			glBindVertexArray(VAO);
+			glDrawElements(GL_TRIANGLES, sizeof(index), GL_UNSIGNED_INT, 0);
+		}
 
 		// ImGui
 		ImGui_ImplOpenGL3_NewFrame();
@@ -241,4 +281,62 @@ void processInput(GLFWwindow* window)
 	{
 		glfwSetWindowShouldClose(window, true);
 	}
+
+	float cameraSpeed = 2.5f * deltaTime;
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+		cameraPos += cameraSpeed * cameraFront;
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+		cameraPos -= cameraSpeed * cameraFront;
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+		cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+		cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+}
+
+void mouse_callback(GLFWwindow* window, double xposIn, double yposIn)
+{
+	float xpos = static_cast<float>(xposIn);
+	float ypos = static_cast<float>(yposIn);
+
+	if (firstMouse)
+	{
+		lastX = xpos;
+		lastY = ypos;
+		firstMouse = false;
+	}
+
+	float xoffset = xpos - lastX;
+	float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+	lastX = xpos;
+	lastY = ypos;
+
+	float sensitivity = 0.1f; // change this value to your liking
+	xoffset *= sensitivity;
+	yoffset *= sensitivity;
+
+	yaw += xoffset;
+	pitch += yoffset;
+
+	// make sure that when pitch is out of bounds, screen doesn't get flipped
+	if (pitch > 89.0f)
+		pitch = 89.0f;
+	if (pitch < -89.0f)
+		pitch = -89.0f;
+
+	glm::vec3 front;
+	front.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+	front.y = sin(glm::radians(pitch));
+	front.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+	cameraFront = glm::normalize(front);
+}
+
+// glfw: whenever the mouse scroll wheel scrolls, this callback is called
+// ----------------------------------------------------------------------
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+	fov -= (float)yoffset;
+	if (fov < 1.0f)
+		fov = 1.0f;
+	if (fov > 45.0f)
+		fov = 45.0f;
 }
